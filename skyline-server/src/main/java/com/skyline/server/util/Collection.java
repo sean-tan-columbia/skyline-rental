d095a1a7cd685644b484b8ee75fda70c563c6e5b
jtan <sean.tan.columbia@gmail.com>
2017-07-14 18:07:04 -0400

    Add method to generate jks for Https server.
    Signed-off-by: jtan <sean.tan.columbia@gmail.com>

--- skyline-server/src/main/java/com/skyline/server/util/Collection.java
+++ skyline-server/src/main/java/com/skyline/server/util/Collection.java
@@ -10,18 +10,29 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.common.collect.Lists;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.JksOptions;
+import sun.security.tools.keytool.CertAndKeyGen;
+import sun.security.x509.X500Name;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by jtan on 6/29/17.
  */
-public class Test {
+public class Collection {
 
     public static void main(String[] args) {
+        generateHttpsServerKeyStore();
+    }
+
+    private static void generateGoogleApisStoredCred() {
         String userId = "sysops";
         try {
             GoogleAuthorizationCodeFlow authorizationFlow = new GoogleAuthorizationCodeFlow.Builder(
@@ -58,6 +69,24 @@ public class Test {
             authorizationFlow.createAndStoreCredential(tokenResponse, userId);
         } catch (IOException e) {
             System.out.println(e);
+        }
+    }
+
+    private static void generateHttpsServerKeyStore() {
+        try {
+            // Generate a self-signed key pair and certificate.
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(null, null);
+            CertAndKeyGen keyPair = new CertAndKeyGen("RSA", "SHA1WithRSA", null);
+            X500Name x500Name = new X500Name("localhost", "Engineering", "Skyline", "New York", "NY", "US");
+            keyPair.generate(1024);
+            PrivateKey privateKey = keyPair.getPrivateKey();
+            X509Certificate[] chain = new X509Certificate[1];
+            chain[0] = keyPair.getSelfCertificate(x500Name, new Date(), (long) 365 * 24 * 60 * 60);
+            keyStore.setKeyEntry("skyline-server-dev", privateKey, "skyline".toCharArray(), chain);
+            keyStore.store(new FileOutputStream("skyline-server-dev.jks"), "skyline".toCharArray());
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException | NoSuchProviderException | InvalidKeyException | SignatureException ex) {
+            Logger.getLogger(Collection.class.getName()).log(Level.SEVERE, "Failed to generate a self-signed cert and other SSL configuration methods failed.", ex);
         }
     }
 
